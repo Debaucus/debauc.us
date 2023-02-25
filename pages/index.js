56535b5b45f58dad99af4b336db682f1f3c8c67f
Mino S <60629996+Debaucus@users.noreply.github.com>
2023-02-25 20:57:53 +0000

    Side by Side article removed. DNDW Update

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,6 @@ import { getAllFilesFrontMatter } from '@/lib/mdx'
 import Card from '@/components/Card'
 import Image from '@/components/Image'
 
-// Math keeps messing up. To be worked on.
-let projects = 0
-
 export async function getStaticProps() {
   const posts = await getAllFilesFrontMatter('blog')
 
@@ -42,7 +39,7 @@ export default function Home({ posts }) {
                 <Image
                   alt="Debaucus Photo"
                   src="/static/images/debaucus_front.jpg"
-                  className="h-60 w-60 rounded-full object-cover object-center md:h-full md:w-full"
+                  className="mx-auto h-60 w-60 rounded-full object-cover object-center md:mb-0 md:h-full md:w-full"
                   width={400}
                   height={400}
                 />
@@ -51,107 +48,101 @@ export default function Home({ posts }) {
           </div>
         </div>
       </section>
-      <div className="" id="active">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Active Projects
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            My current active and running projects.
-          </p>
-        </div>
 
-        <div className="container grid grid-cols-1 md:grid-cols-2">
-          {!posts.length && 'No active projects.'}
-          {posts.slice(0, 100).map((frontMatter) => {
-            const alignStyle =
-              projects % 2 == 0 ? 'md:mr-4' : 'md:mt-[50%] md:mb-[-50%] md:ml-4 last:mb-0'
-            const {
-              slug,
-              date,
-              title,
-              summary,
-              tags,
-              previewImage,
-              websiteURL,
-              websiteKeyword,
-              websiteURLLabel,
-            } = frontMatter
-            if (frontMatter.tags.includes('active')) {
-              projects = projects + 1
-              return (
-                <div key={slug} className={alignStyle}>
-                  <Card
-                    title={title}
-                    date={date}
-                    tags={tags}
-                    description={summary}
-                    imgSrc={previewImage}
-                    href={slug}
-                    websiteURL={websiteURL}
-                    websiteURLLabel={websiteURLLabel}
-                    websiteKeyword={websiteKeyword}
-                  />
-                </div>
-              )
-            } else return
-          })}
-        </div>
-        <div className="flex justify-end text-base font-medium leading-6">
-          <Link
-            href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label="all posts"
-          >
-            All Posts &rarr;
-          </Link>
-        </div>
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Previous Projects
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Projects I have run in the past that have been closed down or completed.
-          </p>
-        </div>
-
-        <div className="container grid grid-cols-1 md:grid-cols-2">
-          {!posts.length && 'No active projects.'}
-          {posts.slice(0, 100).map((frontMatter) => {
-            const alignStyle2 =
-              projects % 2 == 0 ? 'md:mr-4' : 'md:mt-[50%] md:mb-[-50%] md:ml-4 last:mb-0'
-            const {
-              slug,
-              date,
-              title,
-              summary,
-              tags,
-              previewImage,
-              websiteURL,
-              websiteKeyword,
-              websiteURLLabel,
-            } = frontMatter
-            if (frontMatter.tags.includes('inactive')) {
-              projects = projects + 1
-              return (
-                <div key={slug} className={alignStyle2}>
-                  <Card
-                    title={title}
-                    date={date}
-                    tags={tags}
-                    description={summary}
-                    imgSrc={previewImage}
-                    href={slug}
-                    websiteURL={websiteURL}
-                    websiteURLLabel={websiteURLLabel}
-                    websiteKeyword={websiteKeyword}
-                  />
-                </div>
-              )
-            } else return
-          })}
-        </div>
+      <div className="space-y-2 pt-6 pb-8 md:space-y-5" id="active">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Active Projects
+        </h1>
+        <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          My current active and running projects.
+        </p>
       </div>
+
+      <div className="container grid grid-cols-1 md:grid-cols-2">
+        {!posts.length && 'No active projects.'}
+        {posts.slice(0, 100).map((frontMatter) => {
+          const {
+            slug,
+            date,
+            title,
+            summary,
+            tags,
+            previewImage,
+            websiteURL,
+            websiteKeyword,
+            websiteURLLabel,
+          } = frontMatter
+          if (frontMatter.tags.includes('active')) {
+            return (
+              <div key={slug} className="md:mr-4">
+                <Card
+                  title={title}
+                  date={date}
+                  tags={tags}
+                  description={summary}
+                  imgSrc={previewImage}
+                  href={slug}
+                  websiteURL={websiteURL}
+                  websiteURLLabel={websiteURLLabel}
+                  websiteKeyword={websiteKeyword}
+                />
+              </div>
+            )
+          } else return
+        })}
+      </div>
+      <div className="flex justify-end text-base font-medium leading-6">
+        <Link
+          href="/blog"
+          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+          aria-label="all posts"
+        >
+          All Posts &rarr;
+        </Link>
+      </div>
+      <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Previous Projects
+        </h1>
+        <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          Projects I have run in the past that have been closed down or completed.
+        </p>
+      </div>
+
+      <div className="container grid grid-cols-1 md:grid-cols-2">
+        {!posts.length && 'No active projects.'}
+        {posts.slice(0, 100).map((frontMatter) => {
+          const {
+            slug,
+            date,
+            title,
+            summary,
+            tags,
+            previewImage,
+            websiteURL,
+            websiteKeyword,
+            websiteURLLabel,
+          } = frontMatter
+          if (frontMatter.tags.includes('inactive')) {
+            return (
+              <div key={slug} className="md:mr-4">
+                <Card
+                  title={title}
+                  date={date}
+                  tags={tags}
+                  description={summary}
+                  imgSrc={previewImage}
+                  href={slug}
+                  websiteURL={websiteURL}
+                  websiteURLLabel={websiteURLLabel}
+                  websiteKeyword={websiteKeyword}
+                />
+              </div>
+            )
+          } else return
+        })}
+      </div>
+
       <div className="flex justify-end text-base font-medium leading-6">
         <Link
           href="/blog"
