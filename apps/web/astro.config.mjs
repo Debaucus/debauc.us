6d64e694704ad0d7cea95f10308dd728b72ed433
Debaucus <orzotv@gmail.com>
2026-02-06 11:32:38 +0000

    Astro Sitemap. Default robots.txt. Base Head file with all SEO. Blog Posts data filled. Schema Markdown added.

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,11 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://debauc.us',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap()]
 });
